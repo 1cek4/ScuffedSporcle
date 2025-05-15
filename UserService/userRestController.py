6ab267db9c6838fdb830d5d0ae176511d71ace53
1cek4 <67841585+1cek4@users.noreply.github.com>
2025-05-15 15:27:03 -0600

    User dockerization

--- UserService/userRestController.py
+++ UserService/userRestController.py
@@ -1,25 +1,28 @@
+
 import mariadb
+import mysql.connector
+
 import sys
 
 from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 import json
 import requests
-import os
+import os   
 from uuid import UUID
 from dotenv import load_dotenv
 import py_eureka_client.eureka_client as eureka_client
 
 
 try:
-    conn = mariadb.connect(
-        user="db_user",
-        password="db_user_passwd",
-        host="192.0.2.1",
-        port=3306,
-        database="employees"
+    conn = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="abc123",
+    port=3306,
+    database="userdb"
+)
 
-    )
 except mariadb.Error as e:
     print(f"Error connecting to MariaDB Platform: {e}")
     sys.exit(1)
@@ -55,20 +58,19 @@ async def startup_event():
         instance_ip=BASKET_SERVICE_IP
     )
 
-@app.get("")
+@app.get("/Users", response_model=list[User])
 def get_user(user_id: str):
     cur.execute(
-    "SELECT first_name,last_name FROM employees WHERE first_name=?", 
-    (some_name,))
+    "SELECT * FROM users")
     vars = cur.fetchall();
 
-    return basket
+    return user
 
-@app.get("/{user_id}", response_model=User)
+@app.get("/Users/{user_id}", response_model=User)
 def get_user_with_id(user_id: str):
     cur.execute(
-    "SELECT first_name,last_name FROM employees WHERE first_name=?", 
-    (some_name,))
+    "SELECT * FROM users WHERE userGuid =?", 
+    (user_id,))
     vars = cur.fetchall();
 
-    return basket
+    return user
